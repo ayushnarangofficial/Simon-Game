f8a0326965cf6a8391de29122b13adf7a6e7a56f
Ayush Narang <108880041+ayushnarangofficial@users.noreply.github.com>
2022-08-27 22:22:13 +0530

    Update game.js

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).reload(function() {
+$(document).ready(function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
